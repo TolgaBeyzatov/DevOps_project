b9697e76b5798e92c2c9e91ad379485d080afb65
tolga <tolga@Ubuntu.myguest.virtualbox.org>
2024-09-15 19:45:18 +0300

    push to dev

--- counter-service.py
+++ counter-service.py
@@ -8,11 +8,11 @@ COUNTER_FILE = "/data/counter.txt"
 
 def read_counter():
     """
-    Reads and returns the current counter value from the file.
+    Reads and returns the current counter value from the file. test push!!!.
     If the file doesn't exist, returns 0.
 
     Returns:
-        int: The current counter value.
+        int: The current counter value!
     """
     if os.path.exists(COUNTER_FILE):
         with open(COUNTER_FILE, 'r') as f:
